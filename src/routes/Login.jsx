e54b2122f4bd8f819cae9f2d0b171160bc2f3787
Lucas <lucasrs202@gmail.com>
2024-06-02 00:27:50 -0300

    Project migration
    Aside from the previous chat screen, a new login screen was created, and they are connected by React routers, on the App.jsx file.

---
+++ src/routes/Login.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import '../styles/Login.css';
+import { Link } from 'react-router-dom';
+
+export default function Login() {
+  const [username, setUsername] = useState('');
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    setUsername(event.target.elements.username_input.value);
+  };
+
+  return (
+    <div id="container">
+      <header>
+        <h1>Monolog</h1>
+      </header>
+      <p><em>Your private echo chamber.</em></p>
+      <form onSubmit={handleLogin}>
+        <input type="text" id="username_input" placeholder='Pick an username' required />
+        <Link id='link' to={{ pathname: "/chat", state: { username: username } }}>
+          <button id="btn-login">Login</button>
+        </Link>
+      </form>
+    </div>
+  );
+}
